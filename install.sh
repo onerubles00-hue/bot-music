2f05e592503a43f08c365571719fdd7370e7c52b
onerubles00-hue <onerubles00@gmail.com>
2026-05-01 15:37:02 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "🚀 Установка бота..."
+
+# Клонирование проекта
+git clone https://github.com/onerubles00-hue/bot-massage.git
+
+cd bot-massage || exit
+
+# Создание виртуального окружения
+python3 -m venv venv
+
+# Активация
+source venv/bin/activate
+
+# Обновление pip
+pip install --upgrade pip
+
+# Установка зависимостей
+pip install python-telegram-bot APScheduler
+
+echo "✅ Установка завершена"
+echo "👉 Запуск: cd bot-massage && source venv/bin/activate && python bot.py"
